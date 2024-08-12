2ba6d37ff0c3071290044e3e79df66f7e0d322f6
SailorVirgo <tm11solarbeam@gmail.com>
2024-08-12 17:22:06 -0500

    fingers crossed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Adjust if your site is under a subdirectory
-  build: {
-    outDir: 'dist',  // Output directory for the build
-    assetsDir: 'assets',  // Directory for assets within the output directory
-  },
   server: {
     port: 3000,
     open: true,
